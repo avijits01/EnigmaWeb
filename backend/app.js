a17b63ceeeb93ba4939987fcbd3d366e9cca4535
ThatTechedGuy <vgupta463@gmail.com>
2019-12-03 01:02:57 +0530

    firebase integration with enigma and bug fixes

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,12 @@ const firestore = firebase.firestore();
 
 var submitButton = document.getElementById("submit");
 
-const fname = document.getElementById("name");
-const mail = document.getElementById("email");
-const regno = document.getElementById("regno");
-const preference1 = document.getElementById("preference1");
-const preference2 = document.getElementById("preference2");
-const paragraph = document.getElementById("situation");
+const fname = document.querySelector("#name");
+const mail = document.querySelector("#email");
+const regno = document.querySelector("#regno");
+const preference1 = document.querySelector("#preference1");
+const preference2 = document.querySelector("#preference2");
+const paragraph = document.querySelector("#situation");
 
 submitButton.addEventListener("click", function() {
   console.log("Form submitted.");
@@ -32,29 +32,25 @@ submitButton.addEventListener("click", function() {
   } else if (!situation) {
     situation.focus();
   } else {
-    uploadToDatabase();
+    console.log("reached function");
+    var docRef = firestore.collection("FORM_SUBMISSION_DATA").doc(mail.value);
+
+    docRef
+      .set({
+        name: fullName,
+        email: emailid,
+        regNo: registerNo,
+        firstPreference: pref1,
+        secondPreference: pref2,
+        club_contribution: situation
+      })
+      .then(function() {
+        alert(
+          "Thanks! We have received the form. You will now be redirected to the Enigma page"
+        );
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
   }
 });
-
-function uploadToDatabase() {
-  console.log("reached function");
-  var docRef = firestore.collection("FORM_SUBMISSION_DATA").doc(mail.value);
-
-  docRef
-    .set({
-      name: fullName,
-      email: emailid,
-      regNo: registerNo,
-      firstPreference: pref1,
-      secondPreference: pref2,
-      club_contribution: situation
-    })
-    .then(function() {
-      alert(
-        "Thanks! We have received the form. You will now be redirected to the Enigma page"
-      );
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-}
