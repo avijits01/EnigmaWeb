ca7808fcb37d6bf865617e2d58cdc7c2fa7a97a6
ThatTechedGuy <vgupta463@gmail.com>
2019-12-03 20:58:32 +0530

    phone no

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const regno = document.getElementById("regno");
 const preference1 = document.getElementById("preference1");
 const preference2 = document.getElementById("preference2");
 const paragraph = document.getElementById("situation");
+const phoneNumber = document.getElementById("phoneNumber");
 
 //NON-COMPULSORY FIELDS
 const skillset = document.getElementById("skillset");
@@ -34,6 +35,8 @@ function submitForm(e) {
   const pref1 = preference1.value;
   const pref2 = preference2.value;
   const situation = paragraph.value;
+  const phone = phoneNumber.value;
+
   console.log(fullName);
 
   if (!fullName) {
@@ -48,7 +51,9 @@ function submitForm(e) {
     preference2.focus();
   } else if (!situation || situation == "") {
     paragraph.focus();
-  } else {
+  } else if(!phone){
+    phoneNumber.focus();
+  }else {
     console.log("reached function");
     var docRef = firestore.collection("RECRUITMENTS").doc(mail.value);
 
@@ -67,7 +72,8 @@ function submitForm(e) {
         codepenLink: codepen.value,
         otherLinks1: other1.value,
         otherLinks2: other2.value,
-        otherLinks3: other3.value
+        otherLinks3: other3.value,
+        mobile: phone
       })
       .then(function() {
         alert("Thanks! We have received the form.");
