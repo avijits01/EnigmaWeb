cfed84e796fc701547053b211340df9382b20b78
ThatTechedGuy <vgupta463@gmail.com>
2019-12-05 13:36:46 +0530

    config update

--- process.js
+++ process.js
@@ -1,19 +1,18 @@
-const firebase = require("firebase");
-
-var firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.BUCKET,
-  messagingSenderId: process.env.SENDER_ID,
-  appId: process.env.APPLICATION_ID,
-  measurementId: process.env.MEASUREMENT_ID
-};
-
-firebase.initializeApp(firebaseConfig);
-
 async function uploadToFirestore(data) {
+  const firebase = require("firebase");
+
+  var firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.BUCKET,
+    messagingSenderId: process.env.SENDER_ID,
+    appId: process.env.APPLICATION_ID,
+    measurementId: process.env.MEASUREMENT_ID
+  };
+
+  firebase.initializeApp(firebaseConfig);
 
   var status = "FALSE";
   //long running task at https://enigmadev.herokuapp.com/registrations.html
@@ -27,18 +26,19 @@ async function uploadToFirestore(data) {
     .doc(data.email)
     .set(data)
     .then(() => {
-        console.log("DOCUMENT SENT SUCCESSFULLY.");
-        status = "TRUE";
-    }).catch((err) => {
-        console.log("ERROR IN SENDING DOCUMENT" + err);
+      console.log("DOCUMENT SENT SUCCESSFULLY.");
+      status = "TRUE";
+    })
+    .catch(err => {
+      console.log("ERROR IN SENDING DOCUMENT" + err);
     });
 
   return status;
 }
 
 //recieve master process
-process.on('message', async(message) => {
-    const uploaded = await uploadToFirestore(message);
+process.on("message", async message => {
+  const uploaded = await uploadToFirestore(message);
 
-    process.send(uploaded);
+  process.send(uploaded);
 });
