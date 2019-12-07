643921cb3a01a0d17d30b6d239134d3916b505fb
ThatTechedGuy <vgupta463@gmail.com>
2019-12-07 17:48:21 +0530

    serving over https

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function onSubmit(e) {
   //   this.disabled = true;
   // });
 
-  fetch("http://enigmadev.herokuapp/submit", {
+  fetch("https://enigmadev.herokuapp/submit", {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json"
