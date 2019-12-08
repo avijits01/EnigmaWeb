e9735a23bda06cdc278e1ee8f65c458b668dfbbc
Alphamineron <alphamineron@gmail.com>
2019-12-09 05:04:41 +0530

    Added Complete Modular Form Submit Loader with Success and Fail Screens

---
+++ clientJS/Loader.js
@@ -0,0 +1,39 @@
+function showLoader() {
+    $(".SubmissionLoader").show();
+
+    $(".error").hide();
+    $(".status-text-sub").hide();
+    $(".success").hide();
+
+    $(".loader-container").show();
+    $(".status-text").text("Form Submitting");
+}
+
+function showSuccess() {
+    $(".loader-container").hide();
+    $(".error").hide();
+    $(".status-text-sub").hide();
+
+    $(".success").show();
+    $(".status-text").text("Form Submitted!");
+}
+
+function showError() {
+    $(".loader-container").hide();
+    $(".success").hide();
+
+    $(".error").show();
+    $(".status-text-sub").show();
+    $(".status-text").text("Opps! Something Went Wrong.");
+}
+
+function hideLoader() {
+    $(".SubmissionLoader").hide();
+}
+
+$(document).ready(function() {
+    // showLoader();
+    // showSuccess();
+    // showError();
+    // hideLoader();
+});
