269e41ab939ed0a0a5fe017428166eb9a899c4c1
Alphamineron <alphamineron@gmail.com>
2019-12-09 03:24:32 +0530

    Minor Changes

--- clientJS/AiR.js
+++ clientJS/AiR.js
@@ -1,8 +1,10 @@
-var viewportHeader = document.querySelector(".viewport-header");
-
 document.body.addEventListener("scroll", function(event) {
     var opacity = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
     var scale = (document.body.offsetHeight - document.body.scrollTop) / document.body.offsetHeight;
     document.documentElement.style.setProperty('--headerOpacity', opacity);
     document.documentElement.style.setProperty('--headerScale', scale);
+});
+
+$(document).ready(function() {
+    var viewportHeader = document.querySelector(".viewport-header");
 });
