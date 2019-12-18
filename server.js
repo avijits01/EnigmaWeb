b35a197b96ea3f93a42bd3892a8e100f2cd150cc
ThatTechedGuy <vgupta463@gmail.com>
2019-12-19 00:16:52 +0530

    cors trial 2

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Origin", "www.enigmadev.in");
+  res.header("Access-Control-Allow-Origin", "www.enigmadev.in/apply");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
