3db47282de8ea13cf73e77fe6b238a2efe510282
Alphamineron <alphamineron@gmail.com>
2019-12-18 21:52:06 +0530

    Adding Timeline Route

--- server.js
+++ server.js
@@ -66,6 +66,9 @@ app.get("/AiR", function(req, res) {
 app.get("/airesearch", function(req, res) {
   res.sendFile(path.join(__dirname + "/public/pages/AiR.html"));
 });
+app.get("/sMYaa6yxxhprrLHGGmusscYaa6yxxhprrLHGGmusscYaa6yxxhprrLHGGmusscYaa6yxxhprvLkgb", function(req, res) {
+  res.sendFile(path.join(__dirname + "/pages/timeline.html"));
+});
 
 
 
